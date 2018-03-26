002e51fe1d856369ae27b71413db1095236a108c
harithanagaraj <36696201+harithanagaraj@users.noreply.github.com>
2018-03-26 16:42:28 +0530

    prgm
    good

---
+++ bitwiswnum.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main() 
+    int i = 65;
+    int k = 120;
+    printf(" value of i=%d k=%d before swapping", i, k);
+    int(i=0;i++;i>6);
+
+    i = i ^ k;
+    k = i ^ k;
+    i = i ^ k;
+    printf("value of i=%d k=%d after swapping", i, k);
+
+    return 0;
+}
